99031b62912659065784781064b7e01c5b6e5c9c
AAsteriskz7 <avsadarsh7@gmail.com>
2026-05-28 14:35:28 -0700

    finished step and prepcessing for simulation program.

--- src/simulate_tournament.py
+++ src/simulate_tournament.py
@@ -1,6 +1,52 @@
+import pandas as pd
+import numpy as np
+import joblib
+from pathlib import Path
+from collections import defaultdict #like a python dictionary  but more features
+
 # Goal: Simulate the 2026 World Cup 10,000 times using Monte Carlo simulation and trained Poisson models.
 
-# Step 1: Setup & Ingestion
+BASE_DIR = Path(__file__).parent.parent
+MODEL_DIR = BASE_DIR / 'data' / 'models'
+DATA_DIR = BASE_DIR / 'data' / 'processed'
+
+# Load the models we built from the saved folder, ran in prev step. 
+home_model = joblib.load(MODEL_DIR / 'home_poisson.pkl')
+away_model = joblib.load(MODEL_DIR / 'away_poisson.pkl')
+
+GROUPS = {
+    'A': ['Mexico', 'South Africa', 'South Korea', 'Czechia'],
+    'B': ['Canada', 'Bosnia and Herzegovina', 'Qatar', 'Switzerland'],
+    'C': ['Brazil', 'Morocco', 'Haiti', 'Scotland'],
+    'D': ['United States', 'Paraguay', 'Australia', 'Turkey'],
+    'E': ['Germany', 'Curacao', 'Ivory Coast', 'Ecuador'],
+    'F': ['Netherlands', 'Japan', 'Sweden', 'Tunisia'],
+    'G': ['Belgium', 'Egypt', 'Iran', 'New Zealand'],
+    'H': ['Spain', 'Cape Verde', 'Saudi Arabia', 'Uruguay'],
+    'I': ['France', 'Senegal', 'Iraq', 'Norway'],
+    'J': ['Argentina', 'Algeria', 'Austria', 'Jordan'],
+    'K': ['Portugal', 'DR Congo', 'Uzbekistan', 'Colombia'],
+    'L': ['England', 'Croatia', 'Ghana', 'Panama']
+}
+
+def get_current_elos(df): # function to extract elos of each team based on teh groups. 
+    latest_elos = {}
+    for team in df['home_team'].unique():
+        last_home = df[df['home_team'] == team].tail(1) # finds the last time they played
+        if not last_home.empty:
+            latest_elos[team] = last_home['home_elo_post'].values[0]
+            
+    for team in df['away_team'].unique():
+        last_away = df[df['away_team'] == team].tail(1)
+        if not last_away.empty:
+            if team not in latest_elos or last_away['date'].values[0] > last_home['date'].values[0]:
+                latest_elos[team] = last_away['away_elo_post'].values[0]
+                
+    return latest_elos
+
+df = pd.read_csv(DATA_DIR / 'elo_results.csv', parse_dates=['date'])
+current_elos = get_current_elos(df)
+
 
 # Step 2: Core Match Simulation (simulate_match)
 
