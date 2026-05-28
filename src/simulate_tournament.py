53c1bf4fb2717e281f9bb6880f8919e80450e711
AAsteriskz7 <avsadarsh7@gmail.com>
2026-05-28 14:52:46 -0700

    Core Match Simulation logic completed

--- src/simulate_tournament.py
+++ src/simulate_tournament.py
@@ -14,6 +14,8 @@ DATA_DIR = BASE_DIR / 'data' / 'processed'
 home_model = joblib.load(MODEL_DIR / 'home_poisson.pkl')
 away_model = joblib.load(MODEL_DIR / 'away_poisson.pkl')
 
+HOSTS = ['United States', 'Mexico', 'Canada']
+
 GROUPS = {
     'A': ['Mexico', 'South Africa', 'South Korea', 'Czechia'],
     'B': ['Canada', 'Bosnia and Herzegovina', 'Qatar', 'Switzerland'],
@@ -47,8 +49,36 @@ def get_current_elos(df): # function to extract elos of each team based on teh g
 df = pd.read_csv(DATA_DIR / 'elo_results.csv', parse_dates=['date'])
 current_elos = get_current_elos(df)
 
-
 # Step 2: Core Match Simulation (simulate_match)
+def simulate_match(team1, team2, is_knockout=False):
+    swapped = False
+    if team2 in HOSTS and team1 not in HOSTS:
+        team1, team2 = team2, team1
+        swapped = True
+        
+    elo1 = current_elos.get(team1, 1500.0) #sets default o 1500, shouldnt happen tho. 
+    elo2 = current_elos.get(team2, 1500.0)
+    
+    elo_diff = elo1 - elo2
+    
+    neutral_val = 0 if (team1 in HOSTS and team2 not in HOSTS) else 1
+    
+    lambda_1 = home_model.predict([1, elo_diff, neutral_val])[0] #using our created models to predict based on elo diff
+    lambda_2 = away_model.predict([1, -elo_diff, neutral_val])[0]
+    
+    score1 = np.random.poisson(lambda_1)
+    score2 = np.random.poisson(lambda_2)
+    
+    if is_knockout and score1 == score2:
+        prob1 = 0.55 if elo1 > elo2 else 0.45
+        if np.random.rand() < prob1:
+            score1 += 1
+        else:
+            score2 += 1
+            
+    if swapped:
+        return score2, score1
+    return score1, score2
 
 # Step 3: Group Stage Logic (simulate_group_stage)
 
